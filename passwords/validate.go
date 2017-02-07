49247f93c7df5724b986b4e0257ed68f3bf862ba
elpinal <elpinal@users.noreply.github.com>
2017-02-07 16:55:06 +0900

    Reduce allocation as the case may be

--- passwords/validate.go
+++ passwords/validate.go
@@ -3,20 +3,26 @@ package passwords
 
 import "errors"
 
+var (
+	errShort       = errors.New("password is too short (minimum is 8 characters)")
+	errLong        = errors.New("password is too long (maximum is 128 characters)")
+	errIllegalChar = errors.New("password can only contain ASCII printable characters except spaces")
+)
+
 // Validate validates password.
 // If password is valid, this returns nil.
 // In a default setting, minimum length is 8, maximum length is 128, and allowed code points are in the range [33, 126].
 func Validate(password string) error {
 	// TODO: Support specifying settings.
 	if l := len(password); l < 8 {
-		return errors.New("password is too short (minimum is 8 characters)")
+		return errShort
 	} else if l > 128 {
-		return errors.New("password is too long (maximum is 128 characters)")
+		return errLong
 	}
 
 	for _, s := range password {
 		if s < 33 || 126 < s {
-			return errors.New("password can only contain ASCII printable characters except spaces")
+			return errIllegalChar
 		}
 	}
 	return nil
