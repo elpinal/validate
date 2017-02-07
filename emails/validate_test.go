3eb420b867d8e418f4dd08d567bd2a32695700d6
elpinal <elpinal@users.noreply.github.com>
2017-02-07 16:21:56 +0900

    emails: add test

---
+++ emails/validate_test.go
@@ -0,0 +1,45 @@
+package emails
+
+import (
+	"strings"
+	"testing"
+)
+
+var valids = []struct {
+	input string
+}{
+	{"aaa@example.com"},
+	{"aaa+bbb@example.com"},
+	{"AAABBB@example.com"},
+	{"aaa.bbb.ccc@example.com"},
+	{strings.Repeat("a", 64) + "@" + strings.Repeat("b", 254-69) + ".com"},
+}
+
+func TestValidateValids(t *testing.T) {
+	for _, v := range valids {
+		if err := Validate(v.input); err != nil {
+			t.Errorf("Validate(%s): %v", v.input, err)
+		}
+	}
+}
+
+var invalids = []struct {
+	input string
+}{
+	{""},
+	{"@"},
+	{".@.com"},
+	{"aaa.@example.com"},
+	{"aaa bbb@example.com"},
+	{"aaa..bbb..ccc@example.com"},
+	{strings.Repeat("a", 65) + "@" + strings.Repeat("b", 254-70) + ".com"},
+	{strings.Repeat("a", 255-len("@example.com")) + "@example.com"},
+}
+
+func TestValidateInvalids(t *testing.T) {
+	for _, v := range invalids {
+		if err := Validate(v.input); err == nil {
+			t.Errorf("Validate(%s) succeeded unexpectedly", v.input)
+		}
+	}
+}
